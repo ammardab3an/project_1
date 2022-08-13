0b80e2f9ff3a3fac50907d092dca80732c60504e
AmmarDab3an <ammar.dabaan99@gmail.com>
2022-08-13 21:21:57 +0300

    stars + (nav for courses list)

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,23 @@ const groups = [
     'c_draw',
 ];
 
+function stars(rating){
+
+    let ret = [];
+    for(let i = 1; i <= 5; i++){
+        if(rating >= i){
+            ret.push('<i class="fa-solid fa-star"></i>');
+        }
+        else if(rating+0.5>=i){
+            ret.push('<i class="fa-solid fa-star-half-stroke"></i>');
+        }
+        else{
+            ret.push('<i class="fa-regular fa-star"></i>');
+        }
+    }
+    return ret.join('\n');
+}
+
 function load_courses(group, filter_s=''){
     
     fetch(`./data/${group.slice(2)}_res.json`)
@@ -18,7 +35,7 @@ function load_courses(group, filter_s=''){
                 <h2 class="course_title">${data.header}</h2>
                 <p class="course_desc">${data.description}</p>
                 <button class="explore">Explore ${document.getElementById(group).innerText}</button>
-                <ul class="courses_reco">
+                <ul id="courses_reco" class="courses_reco">
                     ${data.courses.filter(course => course.title.toLowerCase().includes(filter_s.toLowerCase())).map(course => (`
                         <li>
                             <figure>
@@ -28,6 +45,9 @@ function load_courses(group, filter_s=''){
                             ${course.instructors.map(instructor => `
                                 <h4 class="author">${instructor.name}</h4>
                             `).join('\n')}
+                            <div class="stars">
+                                ${stars(course.rating)}
+                            </div>
                             <h3 class="price">$${course.price}</h3>
                         </li>
                     `)).join('\n')}
@@ -55,9 +75,9 @@ for (const group of groups){
 const search_btn = document.getElementById("search_submit_btn");
 search_btn.addEventListener("click", (e) => {
     e.preventDefault();
-    document.getElementById("courses").scrollIntoView({behavior: "smooth", block: "center"});
     const filter_s = document.getElementById("search_bar_input").value;
     load_courses(cur, filter_s);
+    document.getElementById("courses_reco").scrollIntoView({behavior: "smooth", block: "start"});
 });
 
 const search_bar = document.getElementById("search_bar_input");
